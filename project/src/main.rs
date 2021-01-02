573bd082164ad35eb75208b1dccb18572b8d2e42
Ryota Bannai <ryotala0528@gmail.com>
2021-01-02 22:01:42 +0900

    add: error handling syntax sugars.

--- project/src/main.rs
+++ project/src/main.rs
@@ -2,11 +2,14 @@
 mod result;
 mod utils;
 
-fn main() {
+fn cast_string() {
     let s1: String = String::from("Hello world");
     let s2: &str = &s1; // String -> &str
     let s3: String = s2.to_string(); // &str -> String
     println!("{}", s3);
-    result::test_result();
+}
+
+fn main() {
+    result::test_result3();
     utils::nested::func();
 }
--- project/src/result/result.rs
+++ project/src/result/result.rs
@@ -5,3 +5,31 @@ pub fn test_result() {
     Err(err) => println!("Err: {}", err),
   }
 }
+
+pub fn test_result2() {
+  let result: Result<i32, String> = Ok(200);
+  if let Ok(code) = result {
+    println!("code: {}", code);
+  }
+}
+
+/*
+  unwrap_or を使うと Ok() だった場合はそのまま展開し、Err() だった場合は引数で与えた値を返す
+*/
+pub fn test_result3() {
+  let result1: Result<i32, String> = Ok(200);
+  println!("code: {}", result1.unwrap_or(-1)); // 200
+
+  let result2: Result<i32, String> = Err("error".to_string());
+  println!("code: {}", result2.unwrap_or(-1)); // 1
+}
+
+// use and_then()
+fn func(code: i32) -> Result<i32, String> {
+  println!("code: {}", code);
+  Ok(100)
+}
+fn func_main() {
+  let result: Result<i32, String> = Ok(200);
+  let next_result = result.and_then(func); // func は実行される
+}
