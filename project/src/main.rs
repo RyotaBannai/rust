afb1841d59acf731b54a9af15d42b559d5cf093c
Ryota Bannai <ryotala0528@gmail.com>
2021-01-02 19:29:40 +0900

    add: fix

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let s1: String = String::from("Hello world");
+    let s2: &str = &s1;                 // String -> &str
+    let s3: String = s2.to_string();    // &str -> String
+    println!("{}", s3);
 }
