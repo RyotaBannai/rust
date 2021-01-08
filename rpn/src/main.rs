71b328ff8fb6a50e60e4cde6bcdc225727a517ac
Ryota Bannai <ryotala0528@gmail.com>
2021-01-08 23:58:43 +0900

    add: clap

--- rpn/src/main.rs
+++ rpn/src/main.rs
@@ -1,3 +1,35 @@
+// use std::env;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    // let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args)
+
+    let mathces = App::new("My RPN program.")
+        .version("1.0.0")
+        .author("Ryota")
+        .about("Super awesome RPN calculator")
+        .arg(
+            Arg::with_name("formula_file")
+                .help("Formulas written in RPN")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .help("Sets the level of verbosity")
+                .value_name("v")
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+
+    match mathces.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    let verbose = mathces.is_present("verbose");
+    println!("Is verbosity specified?:{} ", verbose);
 }
