1f60f7eac20da47806d9adb6b9b4b112481b58c3
Ryota Bannai <ryotala0528@gmail.com>
2021-01-10 22:36:59 +0900

    add: impl add and delete todo

--- actix_web_todo/src/main.rs
+++ actix_web_todo/src/main.rs
@@ -1,13 +1,29 @@
+#[macro_use]
+extern crate log;
 mod app_errors;
-use actix_web::{get, web, App, HttpResponse, HttpServer};
+use actix_web::{get, http::header, middleware::Logger, post, web, App, HttpResponse, HttpServer};
 use app_errors::errors::AppError;
 use askama::Template;
+use async_std::task;
+use env_logger;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use serde::Deserialize;
+use std::env;
 
 struct TodoEntry {
     id: u32,
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
+
+#[derive(Deserialize)] // params: web::Form<AddParams> で自動で deserialize させる
+struct AddParams {
     text: String,
 }
 
@@ -39,6 +55,32 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
         .body(response_body))
 }
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, AppError> {
+    debug!("add request");
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish()) // Ok(...) でトップドメイン / へリダイレクト
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, AppError> {
+    debug!("delete request");
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
     let manager = SqliteConnectionManager::file("todo.db");
@@ -52,10 +94,20 @@ async fn main() -> Result<(), actix_web::Error> {
         params![],
     )
     .expect("Failed to create a table `todo`.");
+
+    env::set_var("RUST_LOG", "actix_web_todo=debug,actix_web=info");
+    env_logger::init();
+
     // ここでコネクションプールを渡す
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
-        .bind("0.0.0.0:8081")?
-        .run()
-        .await?;
+    let app = move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(index)
+            .service(add_todo)
+            .service(delete_todo)
+            .data(pool.clone())
+    };
+    debug!("app started..");
+    HttpServer::new(app).bind("0.0.0.0:8081")?.run().await?;
     Ok(())
 }
--- project/src/utils/sub/general.rs
+++ project/src/utils/sub/general.rs
@@ -1,7 +1,8 @@
 pub fn app_to_string<S: Into<String>>(s: S) -> Option<String> {
   Some(s.into())
 }
-
+// &str <-> String
+// https://qiita.com/uasi/items/3b08a5ba81fede837531
 pub fn test_two_string_type() {
   let s1 = app_to_string("str");
   let s2 = app_to_string("str".to_string());
