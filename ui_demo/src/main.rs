5ec56ad18d088b987dccf51811a7deeb06986695
Ryota Bannai <ryotala0528@gmail.com>
2021-01-15 22:25:46 +0900

    add: add view parts.

--- ui_demo/src/main.rs
+++ ui_demo/src/main.rs
@@ -1,10 +1,23 @@
-use iced::{executor, Application, Command, Element, Settings, Text};
+use iced::{
+    button, executor, Align, Application, Button, Column, Command, Element, Font,
+    HorizontalAlignment, Length, Row, Settings, Subscription, Text,
+};
+
+const FONT: Font = Font::External {
+    name: "PixelMplus12-Regular",
+    bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
+};
 
 fn main() {
-    GUI::run(Settings::default());
+    let mut settings = Settings::default();
+    settings.window.size = (400u32, 120u32);
+    GUI::run(settings);
 }
 
-struct GUI;
+struct GUI {
+    start_stop_button_state: button::State, // ボタンの状態を管理するための変数
+    reset_button_state: button::State,
+}
 
 // https://docs.rs/iced/0.2.0/iced/trait.Application.html
 impl Application for GUI {
@@ -14,7 +27,13 @@ impl Application for GUI {
 
     // Application を run した時に実行した時に呼ばれるメソッド
     fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
-        (GUI, Command::none())
+        (
+            GUI {
+                start_stop_button_state: button::State::new(),
+                reset_button_state: button::State::new(),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
@@ -26,6 +45,37 @@ impl Application for GUI {
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello, World!").into()
+        // Text::new("Hello, World!").into() // 全ての wedged は Into<Element> が実装されているため、into() メソッドで Element に変換できる
+        let tick_text = Text::new("00:00:00").font(FONT).size(60);
+        let start_stop_button = Button::new(
+            &mut self.start_stop_button_state,
+            Text::new("Start")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+
+        let reset_button = Button::new(
+            &mut self.reset_button_state,
+            Text::new("Reset")
+                .horizontal_alignment(HorizontalAlignment::Center)
+                .font(FONT),
+        )
+        .min_width(80);
+
+        Column::new()
+            .push(tick_text)
+            .push(
+                Row::new()
+                    .push(start_stop_button)
+                    .push(reset_button)
+                    .spacing(10),
+            )
+            .spacing(10)
+            .padding(10)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .align_items(Align::Center) // 中央寄せ
+            .into()
     }
 }
