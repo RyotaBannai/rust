43082659084df7c120a01bfc0a3037d0c230650a
Ryota Bannai <ryotala0528@gmail.com>
2021-01-22 23:46:28 +0900

    add: threads notes.

--- panic/experiment/src/main.rs
+++ panic/experiment/src/main.rs
@@ -35,11 +35,18 @@ fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
 
+//https://doc.rust-lang.org/std/thread/
+// yield_now():
+// For example when polling on a resource, it is common to check that it is available, and if not to yield in order to avoid busy waiting.
+// Thus the pattern of yielding after a failed poll is rather common when implementing low-level shared resources or synchronization primitives.
+// However programmers will usually prefer to use channels, Condvars, Mutexes or join for their synchronization routines, as they avoid thinking about thread scheduling.
+
 fn thread_has_a_panic_handling() {
     // スレッドのパニックの有無は、JoinHandle::join の Result の中に入っている
     // => スレッド自体が catch_unwind と同等の機能を持っていると考えられる（thread では panic 処理をする必要がない）
     let t1 = thread::spawn(|| assert!(false));
     // eprintln! macro is used for io::stderr or progress messages.
+    // join を呼ぶとそのスレッドが完了するまで、呼び出し側が wait 状態になる
     eprintln!("is_ok = {}", t1.join().is_ok()); // false
 
     let t2 = thread::spawn(|| assert!(true));
