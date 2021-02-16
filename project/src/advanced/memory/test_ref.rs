5a5f08f9dcc020a4b008f76832c4f98b2fc1b941
Ryota Bannai <ryotala0528@gmail.com>
2021-02-16 23:59:00 +0900

    add: added few mores.

--- project/src/advanced/memory/test_ref.rs
+++ project/src/advanced/memory/test_ref.rs
@@ -5,10 +5,24 @@ pub fn test() {
   {
     let age_ref: Ref<u32> = age.borrow();
     println!("{}", *age_ref);
+
+    // or you can drop manually
+    // std::mem::drop(age_ref);
   }
 
   let mut age_mut_ref: RefMut<u32> = age.borrow_mut(); // do not forget mut even if you use RefMut
   *age_mut_ref += 1;
-
   println!("{}", *age_mut_ref);
+
+  let mut age_mut_reference: &u32 = &mut age_mut_ref;
+  println!("{}", *age_mut_reference);
+
+  // inner block
+  // you can't return reference, bc when goes out of block, the age_ref will be dropped.
+  // let age_reference: &u32 = {
+  //   let age_ref: Ref<u32> = age.borrow();
+  //   &age_ref
+  //   // but returning &age.borrow() will work as known as temporary lifetime extension
+  //   // https://doc.rust-lang.org/reference/destructors.html?highlight=temporary,life#temporary-lifetime-extension
+  // };
 }
