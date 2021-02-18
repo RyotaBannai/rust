5028037b62e84dc1c2230ce51d1ba3c568f689f8
Ryota Bannai <ryotala0528@gmail.com>
2021-02-19 00:12:19 +0900

    fix: fixes.

--- project/src/archive/oop/demo.rs
+++ project/src/archive/oop/demo.rs
@@ -11,27 +11,39 @@ impl AverageCollection {
     }
   }
 
-  fn push(&mut self, new_num: i32) {
+  fn push(&mut self, new_num: i32) -> Vec<i32> {
     self.list.push(new_num);
     self.update_average();
+    self.list.clone()
+  }
+
+  fn push_all(&mut self, another_vec: &mut Vec<i32>) -> Vec<i32> {
+    self.list.append(another_vec);
+    self.update_average();
+    self.list.clone()
   }
 
   fn remove(&mut self, nth: usize) -> i32 {
-    self.list.remove(nth)
+    let removed_item = self.list.remove(nth);
+    self.update_average();
+    removed_item
   }
 
   fn remove_first_value(&mut self, value: i32) -> i32 {
-    self.list.remove(
+    let removed_item = self.list.remove(
       self
         .list
         .iter()
         .position(|x| *x == value)
         .expect("this value not found"),
-    )
+    );
+    self.update_average();
+    removed_item
   }
 
   fn remove_all_values(&mut self, value: i32) {
     self.list.retain(|x| *x != value);
+    self.update_average();
   }
 
   fn update_average(&mut self) {
@@ -39,7 +51,7 @@ impl AverageCollection {
     self.average = total as f64 / self.list.iter().len() as f64;
   }
 
-  fn get_averate(&mut self) -> f64 {
+  fn get_average(&mut self) -> f64 {
     self.average
   }
 }
@@ -49,5 +61,11 @@ pub fn test() {
   ac.push(1);
   ac.push(2);
   ac.push(3);
-  println!("{}", ac.get_averate());
+  println!("{}", ac.get_average());
+
+  ac.push_all(&mut vec![4, 2, 3]);
+  println!("{}", ac.get_average());
+
+  ac.remove_all_values(2);
+  println!("{}", ac.get_average());
 }
